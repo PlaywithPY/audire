ae8c685b11cd263b8b4adfdff158a79d6c69e9e3
PlaywithPY <playwithpy@gmail.com>
2026-01-15 17:55:04 +0100

    Refresh site styling to lighter theme

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// ====== CONFIG (à personnaliser) ======
+window.AUDIRE_CONFIG = {
+  phoneDisplay: "04 233 61 25",
+  phoneHref: "+3242336125",
+  email: "contact@audire.be",
+  baseUrl: "https://audire.be" // <-- remplace quand le domaine est prêt
+};
