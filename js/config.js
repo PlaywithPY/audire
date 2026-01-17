e764265c831064869b3da379b0f1f8db039c3bfc
PlaywithPY <playwithpy@gmail.com>
2026-01-17 18:04:39 +0100

    Add shared menu and update contact email

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 window.AUDIRE_CONFIG = {
   phoneDisplay: "04 233 61 25",
   phoneHref: "+3242336125",
-  email: "contact@audire.be",
+  email: "centre.audire@gmail.com",
   baseUrl: "https://audire.be" // <-- remplace quand le domaine est prêt
 };
