770615ec59be3ba32408b255b531b197e8a4d228
PlaywithPY <playwithpy@gmail.com>
2026-05-09 16:43:52 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
         <ThemeProvider />
       </head>
       <body className={`${inter.variable} ${playfair.variable} antialiased`}>
+        <EditorNavSync />
         <Providers>
           <CentreProvider>
             <ConditionalLayout>
